cf8ca529dfdd7ae228366330b4dd7afd1a73ad14
VincentNeumann <vincent.neumann@pub.tech>
2023-02-22 11:56:55 +0100

    simple website check b1 b3

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import requests
+import json
+from datetime import datetime, timedelta
 
-x = requests.get('https://mcdn-a.akamaihd.net/br/hf/7t/')
+x = requests.get('https://mcdn-a.akamaihd.net/br/hf/7t/b1/')
 
-print(x.content)
+#bayern1 testen
+
+URL="https://mcdn-a.akamaihd.net/br/hf/7t/"
+#https://mcdn-a.akamaihd.net/br/hf/7t/b1/b1_20230213T010000+0100.mp4
+#b1_YYYYMMDDTHH0000+0100.mp4
+
+def checkStation1hourago(station):
+    now = datetime.now() - timedelta(hours=1)
+    current_time = now.strftime("%Y:%m:%dT%H0000+0100.mp4")
+
+    filename = ""
+    filename = station + "/" + station + "_" 
+    filename = filename + str(current_time).replace(":","")
+
+    print(URL + filename)
+    x = requests.get(URL + filename)
+
+    if (x.status_code == 200):
+        #success
+        print("success")
+    else:
+        print("error")
+        #slack nachricht
+
+checkStation1hourago("b1")
+checkStation1hourago("b3")
+# print(x.content)
 
